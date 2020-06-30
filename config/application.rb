e8f22b09326656dde7af0a804730a15202d1dc59
jeferbc <jeffe.bernal@gmail.com>
2020-06-30 17:22:59 -0500

    Landing

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module Palamon
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-
+    config.hosts << '7e18c12b.ngrok.io'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
